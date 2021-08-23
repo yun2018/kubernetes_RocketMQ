5a32225bfca0d105dc09e37ae12adea0ab8c0f33
yun2018 <35438956+yun2018@users.noreply.github.com>
2021-08-23 10:56:51 +0800

    1
    1

--- rocketMQ/broker-run-pro.sh
+++ rocketMQ/broker-run-pro.sh
@@ -7,8 +7,8 @@ myConf=/opt/rocketmq-4.4.0/conf/my.properties
 cp /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre/lib/ext/sunjce_provider.jar /opt/rocketmq-4.4.0/lib/
 rm -f ${myConf}
 cp '/opt/rocketmq-4.4.0/conf/broker.conf' ${myConf}
-#echo "brokerIP1=192.168.200.31" >> ${myConf}
-#echo -e "listenPort=30022" >> ${myConf}
+#echo "brokerIP1=172.10.2.31" >> ${myConf}
+#echo -e "listenPort=30002" >> ${myConf}
 cat ${myConf}
 sh mqbroker -n service-rocketmq-ns.rocketmq:9876 -c ${myConf}
 
